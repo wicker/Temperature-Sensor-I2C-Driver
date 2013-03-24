a20ade522d91fae39637915ee1b0e803b8a48e68
Jenner Hanni <jeh.wicker@gmail.com>
2013-03-24 10:41:07 -0700

    #MORE made no difference. Try an #ACK and THEN an IDBR read.

--- temp-sensor-driver.s
+++ temp-sensor-driver.s
@@ -174,15 +174,16 @@ BTN_SVC:
 	MOV R1, #START		@ Load the value for START
 	STR R1, [R0]		@ Write to ICR
 	BL POLLTB
-	LDR R0, =ICR		@ Point to ICR
-	MOV R1, #MORE		@ Load the value to send the pointer
-	STR R1, [R0]		@ Write to ICR
+	@This MORE doesn't make a difference
+	@LDR R0, =ICR		@ Point to ICR
+	@MOV R1, #MORE		@ Load the value to send the pointer
+	@STR R1, [R0]		@ Write to ICR
 	@BL POLLTB
-	LDR R0, =IDBR		@ Point to IDBR
-	LDR R3, [R0]		@ Save the read temperature byte in R3
 	LDR R0, =ICR		@ Point to ICR
 	MOV R1, #ACK		@ Load the value to acknowledge the byte received
 	STR R1, [R0]		@ Write to ICR
+	LDR R0, =IDBR		@ Point to IDBR
+	LDR R3, [R0]		@ Save the read temperature byte in R3
 
 	LDR R0, =ICR		@ Point to ICR
 	MOV R1, #STOP		@ Load the value for STOP
