1ec014bc63a2cd0ec38e4a622923a820e216e168
Jenner Hanni <jeh.wicker@gmail.com>
2013-03-24 10:32:12 -0700

    Really following timing diagram this time.

--- temp-sensor-driver.s
+++ temp-sensor-driver.s
@@ -178,12 +178,11 @@ BTN_SVC:
 	@MOV R1, #MORE		@ Load the value to send the pointer
 	@STR R1, [R0]		@ Write to ICR
 	@BL POLLTB
+	LDR R0, =IDBR		@ Point to IDBR
+	LDR R3, [R0]		@ Save the read temperature byte in R3
 	LDR R0, =ICR		@ Point to ICR
 	MOV R1, #ACK		@ Load the value to acknowledge the byte received
 	STR R1, [R0]		@ Write to ICR
-	LDR R0, =IDBR		@ Point to IDBR
-	LDR R3, [R0]		@ Save the read temperature byte in R3
-	BL POLLTB
 	LDR R0, =IDBR		@ Point to IDBR
 	LDR R4, [R0]		@ Save the read temperature byte in R1
 	AND R4, #0x80		@ Retain only the value in bit 7
